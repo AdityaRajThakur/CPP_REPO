cd405d96cef04f1a7a738182f7ed7d3e0d1552d4
unknown <adityaraj715426@gmail.com>
2023-03-11 15:47:17 +0530

    added sieve to check prime or not in log(n)

---
+++ sieve.cpp
@@ -0,0 +1,24 @@
+#include<iostream> 
+#include<bits/stdc++.h>
+using namespace std ; 
+
+
+// Sieve of Eratosthenes
+
+
+bitset<1000000> Primes;
+void sieve(int n)
+{
+    Primes[0] = 1;
+    for (int i = 3; i*i <= n; i += 2) {
+        if (Primes[i / 2] == 0) {
+            for (int j = 3 * i; j <= n; j += 2 * i)
+                Primes[j / 2] = 1;
+        }
+    }
+}
+
+
+//take o(nloglogn) 
+//to check the n is prime or not 
+// if(n % 2 == 1 && Primes[n / 2] == 0) cout<<n<<endl; 
