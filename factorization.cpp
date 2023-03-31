7d22a2a8e3658c74b7dd6a8750b15fdc80850760
unknown <adityaraj715426@gmail.com>
2023-03-31 09:33:23 +0530

    added to find the factor of number in O(sqrt(n))

---
+++ factorization.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std ;
+void factor(vector<int> &arr , int n){
+    int i = 2 ; 
+    while(i*i<=n){
+        if(n % i ==0){
+            arr.push_back(i); 
+            if(i * i != n){
+                arr.push_back(n / i) ; 
+            }
+        }
+        i++; 
+    }
+    if(n>1) arr.push_back(n) ; 
+}
+int main(){
+    int n ; 
+    cin>>n; 
+
+    vector<int> fact ; 
+    factor(fact , n) ; 
+    for(auto x : fact){
+        cout<<x<<" " ; 
+    }
+    cout<<endl ; 
+}
