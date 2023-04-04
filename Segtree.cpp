e6da315fffa3b8f1b833962a5ec2fe8a8b4e4356
unknown <adityaraj715426@gmail.com>
2023-04-04 19:54:38 +0530

    added the generic segment tree

--- Segtree.cpp
+++ Segtree.cpp
@@ -1,4 +1,21 @@
 #define ll long long
+//---------------------------------------
+struct update{
+    ll setval ; 
+    ll increment ; 
+    update(){
+        setval = -1 ; 
+        increment = 0 ; 
+    }
+    update(ll setval , ll increment){
+        this->setval = setval ; 
+        this->increment  = increment ; 
+    }
+    bool operator==(const update& other)const {
+        return setval == other.setval && increment==other.increment ; 
+    }
+}; 
+//---------------------------------------
 template<class T, class U>
 // T -> node, U->update.
 struct Lsegtree{
@@ -16,8 +33,7 @@ struct Lsegtree{
         for all x        
     */
 
-    Lsegtree(ll n, T identity_element, U identity_update)
-    {
+    Lsegtree(ll n, T identity_element, U identity_update){
         this->n = n;
         this->identity_element = identity_element;
         this->identity_update = identity_update;
@@ -26,18 +42,31 @@ struct Lsegtree{
     }
 
 
-    T combine(T l, T r)
-    {
+//---------------------------------------------------------------------------------------------
+// change these function 
+    T combine(T l, T r){
         // change this function as required.
         T ans = (l + r);
         return ans;
     }
 
+    // change the following 2 functions, and you're more or less done.
+    T apply(T curr, U upd, ll tl, ll tr){
+        T ans = (tr-tl+1)*upd;
+        // increment range by upd:
+        // T ans = curr + (tr - tl + 1)*upd
 
-    void buildUtil(ll v, ll tl, ll tr, vector<T>&a)
-    {
-        if(tl == tr)
-        {
+        return ans;
+    }
+
+    U combineUpdate(U old_upd, U new_upd, ll tl, ll tr){
+        U ans = old_upd;
+        ans=new_upd;
+        return ans;
+    }  
+//---------------------------------------------------------------------------------------------
+    void buildUtil(ll v, ll tl, ll tr, vector<T>&a){
+        if(tl == tr){
             st[v] = a[tl];
             return;
         }
@@ -49,64 +78,40 @@ struct Lsegtree{
 
 
 
-    // change the following 2 functions, and you're more or less done.
-    T apply(T curr, U upd, ll tl, ll tr)
-    {
-        T ans = (tr-tl+1)*upd;
-        // increment range by upd:
-        // T ans = curr + (tr - tl + 1)*upd
-
-        return ans;
-    }
-
-    U combineUpdate(U old_upd, U new_upd, ll tl, ll tr)
-    {
-        U ans = old_upd;
-        ans=new_upd;
-        return ans;
-    }  
 
 
-    void push_down(ll v, ll tl, ll tr)
-    {
+    void push_down(ll v, ll tl, ll tr){
         //for the below line to work, make sure the "==" operator is defined for U.
         if(lazy[v] == identity_update)return;
         st[v] = apply(st[v], lazy[v], tl, tr);
-        if(2*v + 1 <= 4*n)
-        {
+        if(2*v + 1 <= 4*n){
             ll tm = (tl + tr)>>1;
             lazy[2*v + 1] = combineUpdate(lazy[2*v+1], lazy[v], tl, tm);
             lazy[2*v + 2] = combineUpdate(lazy[2*v+2], lazy[v], tm+1,tr);            
         }
         lazy[v] = identity_update;
     }
-    T queryUtil(ll v, ll tl, ll tr, ll l, ll r)
-    {
+    T queryUtil(ll v, ll tl, ll tr, ll l, ll r){
         push_down(v,tl,tr);
         if(l > r)return identity_element;
-        if(tr < l or tl > r)
-        {
+        if(tr < l or tl > r){
             return identity_element;
         }
-        if(l <= tl and r >= tr)
-        {
+        if(l <= tl and r >= tr){
             return st[v];
         }
         ll tm = (tl + tr)>>1;
         return combine(queryUtil(2*v+1,tl,tm,l,r), queryUtil(2*v+2,tm+1,tr,l,r));
     }
  
-    void updateUtil(ll v, ll tl, ll tr, ll l, ll r, U upd)
-    {
+    void updateUtil(ll v, ll tl, ll tr, ll l, ll r, U upd){
         push_down(v,tl,tr); 
         if(tr < l or tl > r)return;
-        if(tl >=l and tr <=r)
-        {
+        if(tl >=l and tr <=r){
             lazy[v] = combineUpdate(lazy[v],upd,tl,tr);
             push_down(v,tl,tr);
         }
-        else
-        {
+        else{
             ll tm = (tl + tr)>>1;
             updateUtil(2*v+1,tl,tm,l,r,upd);
             updateUtil(2*v+2,tm+1,tr,l,r,upd);
@@ -116,17 +121,14 @@ struct Lsegtree{
 
 
 
-    void build(vector<T>a)
-    {
+    void build(vector<T>a){
         assert( (ll)a.size() == n);
         buildUtil(0,0,n-1,a);
     }
-    T query(ll l, ll r)
-    {
+    T query(ll l, ll r){
         return queryUtil(0,0,n-1,l,r);
     }
-    void update(ll l,ll r, U upd)
-    {
+    void update(ll l,ll r, U upd){
         updateUtil(0,0,n-1,l,r,upd);
     }
 };
